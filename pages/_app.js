0aec8217abcbc319f24085397d9c534b59d80109
MusicDev33 <smccowan33@gmail.com>
2022-05-02 15:16:25 -0700

    fix for Vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Layout } from 'components/Layout/layout.js';
+import { Layout } from 'components/layout/layout.js';
 
 function MyApp({ Component, pageProps }) {
   return (
