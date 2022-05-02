3e22091da0b64bc64c6875bb7be3625dc2410773
MusicDev33 <smccowan33@gmail.com>
2022-05-02 14:14:48 -0700

    layout changes and more init

--- components/Layout/layout.js
+++ components/Layout/layout.js
@@ -1,10 +1,23 @@
 import RNavbar from "components/RNavbar/rnavbar";
 
+import styles from './layout.module.scss';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export const Layout = ({ children }) => {
   return (
-    <>
-      <RNavbar />
-      <main>{children}</main>
-    </>
+    <Container fluid className={styles['app-container']}>
+      <Row>
+        <Col md={1}>
+          <RNavbar />
+        </Col>
+
+        <Col md={9}>
+          <main>{children}</main>
+        </Col>
+      </Row>
+    </Container>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '../styles/globals.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Layout } from 'components/Layout/layout';
 
