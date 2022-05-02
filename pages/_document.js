9be2331155d58b3fd264da23e0bb4e3b601e4760
MusicDev33 <smccowan33@gmail.com>
2022-05-02 16:17:55 -0700

    New fonts, new layout

---
+++ pages/_document.js
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Reads</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link 
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@400;500;600&family=Nunito:wght@300;400;500&display=swap" 
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,16 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <Head>
+        <title>Reads</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link 
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@400;500;600&family=Nunito:wght@300;400;500&display=swap" 
+          rel="stylesheet" />
+      </Head>
+    </div>
   );
 }
