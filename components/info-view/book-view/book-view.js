aa81079d664072ec8f9c6290ffa134ad34eb9b36
MusicDev33 <smccowan33@gmail.com>
2022-05-05 21:26:45 -0700

    description field

--- components/info-view/book-view/book-view.js
+++ components/info-view/book-view/book-view.js
@@ -1,6 +1,34 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import styles from './book-view.module.scss';
+
 const BookView = ({book}) => {
   return (
-    <div>{book.title}</div>
+    <div>
+      <div className={styles.titleCard}>
+        <Container>
+          <Row>
+            <Col>
+              <span className={styles.title}>{book.title}</span>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <div className={styles.author}>by {book.author}</div>
+            </Col>
+          </Row>
+
+          <Row className="mt-2">
+            <Col>
+              <textarea className={styles.descText}></textarea>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   );
 }
 
