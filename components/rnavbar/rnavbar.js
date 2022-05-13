f8b06e9ff9309431a72ed8da69638563df0db230
MusicDev33 <smccowan33@gmail.com>
2022-05-12 22:06:57 -0700

    navbar highlighting

--- components/rnavbar/rnavbar.js
+++ components/rnavbar/rnavbar.js
@@ -5,6 +5,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { IconContext } from 'react-icons';
 import { FaBook, FaUserAlt, FaBookmark } from 'react-icons/fa';
@@ -12,13 +13,15 @@ import { FaBook, FaUserAlt, FaBookmark } from 'react-icons/fa';
 const RNavbar = () => {
   const rowClass = '';
 
+  const router = useRouter();
+
   return (
     <Container className={styles.rnavbar + ' py-5 px-0'}>
       <Row className={rowClass}>
         <Col className="text-center">
           <Link href="/books">
             <a>
-              <IconContext.Provider value={{ className: styles.navbarIcon }}>
+              <IconContext.Provider value={{ className: `${styles.navbarIcon} ${router.pathname == '/books' ? styles.active : ''}` }}>
                 <FaBook />
               </IconContext.Provider>
             </a>
