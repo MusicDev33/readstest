a6c6566eaebcf04428fc9f641066de8a0fe27cba
MusicDev33 <smccowan33@gmail.com>
2022-05-13 16:36:07 -0700

    get book by id on synopsis

--- pages/synopsis/[bookid].js
+++ pages/synopsis/[bookid].js
@@ -9,29 +9,54 @@ import Col from 'react-bootstrap/Col';
 import { getBookById } from 'services/book.service';
 import { getSynopsisByBookId } from 'services/synopsis.service';
 
+import styles from './synopsis.module.scss';
+
 const Synopsis = () => {
   const router = useRouter();
   const { bookid } = router.query;
 
-  const [book, setBook] = useState();
-  const [synopsis, setSynopsis] = useState();
+  const [book, setBook] = useState({
+    pages: 0,
+    author: '',
+    title: '',
+    finished: false
+  });
+  const [synopsis, setSynopsis] = useState({
+    bookId: '',
+    description: ''
+  });
 
   const getBook = async () => {
     const bookData = await getBookById(bookid);
 
     console.log(bookData);
+
+    setBook(bookData.data);
   }
 
   useEffect(() => {
 
-    if (bookid) {
+    // @Next, why do I have to do this??? ughhh
+    if (router.isReady) {
       getBook();
     }
-  });
+  }, [router.isReady]);
 
   return (
     <div>
       <Container>
+        <Row>
+          <Col>
+            <div className={styles.title}>{book.title}</div>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <div className={styles.author}>{book.author}</div>
+          </Col>
+        </Row>
+
         <Row>
           <Col>
             { bookid }
