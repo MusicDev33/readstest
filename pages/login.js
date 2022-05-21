e75e79001a0420937878b4b07f24a9821e4a8627
MusicDev33 <smccowan33@gmail.com>
2022-05-20 19:31:02 -0700

    Nice animations for password field

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,38 @@
+import { useState } from 'react';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import { getAuthToken } from 'services/auth.service';
+
 const Login = () => {
+  const defaultPassClass = 'md-form-1 mt-5';
+
+  const [password, setPassword] = useState('');
+  const [passFieldClass, setPassFieldClass] = useState(defaultPassClass);
+
   return (
     <div>
       <Container>
         <Row className="justify-content-center">
           <Col xs={6}>
-            <input type="password" className="md-form-1 mt-5" placeholder="Enter Password" />
+            <input type="password" value={password} onChange={e => {setPassword(e.target.value)}} className={passFieldClass} placeholder="Enter Password"
+              onKeyPress={e => {
+                if (e.key === 'Enter') {
+                  e.preventDefault();
+                  if (passFieldClass !== defaultPassClass) {
+                    setPassFieldClass(defaultPassClass);
+                  }
+
+                  getAuthToken(password).then(res => {
+                    if (!res.success) {
+                      setPassFieldClass(`${defaultPassClass} fail shake-anim`);
+                    }
+                    console.log(res.data);
+                  });
+                }
+              }} />
           </Col>
         </Row>
       </Container>
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { BACKEND } from 'env';
 
-export const getAuthtoken = async (password) => {
+export const getAuthToken = async (password) => {
   let returnData;
 
   const body = {
