5c26e6bdcd0cc0119cb65fafac7243555c2d1ea0
MusicDev33 <smccowan33@gmail.com>
2022-05-31 19:24:56 -0700

    remove config test

--- next.config.js
+++ next.config.js
@@ -6,20 +6,6 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles'), path.join(__dirname, 'components')],
   },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: `/api/v2/:path*`,
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: `https://www.shelbymccowan.com` },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
