95c3fda7402f84cdd8e148b23c01052423dd8296
MusicDev33 <smccowan33@gmail.com>
2022-05-31 18:42:37 -0700

    test config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+// import { BACKEND } from 'env';
+require('dotenv');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -6,6 +8,20 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles'), path.join(__dirname, 'components')],
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: `/api/v2/:path*`,
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: `${process.env.BACKEND}` },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
